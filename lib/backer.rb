9edc559df64368a9c11ae8e5c7102dc79e6f9b99
DANIELLE PARK <dpark1008@gmail.com>
2019-01-19 04:48:20 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,10 +2,10 @@ class Backer
   attr_accessor :name
   def initialize(name)
     @name = name
-    @back_project = []
+    @back_projects = []
   end
 
   def back_project(project)
-    @back_project << project
+    @back_projects << project
   end
 end
